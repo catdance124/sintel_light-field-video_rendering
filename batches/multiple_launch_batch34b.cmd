6be4890153ea8d443103cbc156b1abd1c7a4ec47
catdance124 <kinosi.cat@gmail.com>
2020-07-24 11:05:41 +0900

    add batches

--- batches/multiple_launch_batch34b.cmd
+++ batches/multiple_launch_batch34b.cmd
@@ -4,17 +4,17 @@
 
 @echo off
 cd ..
-set file_path="../clean_files/scenes/05.2_bamboo/05.2b_comp.blend"
+set file_path="../clean_files/scenes/03.4_chickenrun/03.4b_comp.blend"
 echo %file_path%
-set start_frame=81
-set end_frame=130
+set start_frame=67
+set end_frame=116
 
-REM start "0" python .\run_blender.py %file_path% -resume "0 0" -end "0 8" -fs %start_frame% -fe %end_frame%
+start "03.4_chickenrun,03.4b_comp" python .\run_blender.py %file_path% -resume "0 0" -end "8 8" -fs %start_frame% -fe %end_frame%
 REM start "1" python .\run_blender.py %file_path% -resume "1 0" -end "1 8" -fs %start_frame% -fe %end_frame%
 REM start "2" python .\run_blender.py %file_path% -resume "2 0" -end "2 8" -fs %start_frame% -fe %end_frame%
 REM start "3" python .\run_blender.py %file_path% -resume "3 0" -end "3 8" -fs %start_frame% -fe %end_frame%
 REM start "4" python .\run_blender.py %file_path% -resume "4 0" -end "4 8" -fs %start_frame% -fe %end_frame%
 REM start "5" python .\run_blender.py %file_path% -resume "5 0" -end "5 8" -fs %start_frame% -fe %end_frame%
-start "6" python .\run_blender.py %file_path% -resume "6 0" -end "6 8" -fs %start_frame% -fe %end_frame%
+REM start "6" python .\run_blender.py %file_path% -resume "6 0" -end "6 8" -fs %start_frame% -fe %end_frame%
 REM start "7" python .\run_blender.py %file_path% -resume "7 0" -end "7 8" -fs %start_frame% -fe %end_frame%
 REM start "8" python .\run_blender.py %file_path% -resume "8 0" -end "8 8" -fs %start_frame% -fe %end_frame%
--- batches/multiple_launch_batch52a.cmd
+++ batches/multiple_launch_batch52a.cmd
@@ -9,12 +9,12 @@ echo %file_path%
 set start_frame=21
 set end_frame=70
 
-start "0" python .\run_blender.py %file_path% -resume "0 0" -end "0 8" -fs %start_frame% -fe %end_frame%
-start "1" python .\run_blender.py %file_path% -resume "1 0" -end "1 8" -fs %start_frame% -fe %end_frame%
-start "2" python .\run_blender.py %file_path% -resume "2 0" -end "2 8" -fs %start_frame% -fe %end_frame%
-start "3" python .\run_blender.py %file_path% -resume "3 0" -end "3 8" -fs %start_frame% -fe %end_frame%
+REM start "0" python .\run_blender.py %file_path% -resume "0 0" -end "0 8" -fs %start_frame% -fe %end_frame%
+REM start "1" python .\run_blender.py %file_path% -resume "1 0" -end "1 8" -fs %start_frame% -fe %end_frame%
+REM start "2" python .\run_blender.py %file_path% -resume "2 0" -end "2 8" -fs %start_frame% -fe %end_frame%
+REM start "3" python .\run_blender.py %file_path% -resume "3 0" -end "3 8" -fs %start_frame% -fe %end_frame%
 start "4" python .\run_blender.py %file_path% -resume "4 0" -end "4 8" -fs %start_frame% -fe %end_frame%
-start "5" python .\run_blender.py %file_path% -resume "5 0" -end "5 8" -fs %start_frame% -fe %end_frame%
-start "6" python .\run_blender.py %file_path% -resume "6 0" -end "6 8" -fs %start_frame% -fe %end_frame%
-start "7" python .\run_blender.py %file_path% -resume "7 0" -end "7 8" -fs %start_frame% -fe %end_frame%
-start "8" python .\run_blender.py %file_path% -resume "8 0" -end "8 8" -fs %start_frame% -fe %end_frame%
+REM start "5" python .\run_blender.py %file_path% -resume "5 0" -end "5 8" -fs %start_frame% -fe %end_frame%
+REM start "6" python .\run_blender.py %file_path% -resume "6 0" -end "6 8" -fs %start_frame% -fe %end_frame%
+REM start "7" python .\run_blender.py %file_path% -resume "7 0" -end "7 8" -fs %start_frame% -fe %end_frame%
+REM start "8" python .\run_blender.py %file_path% -resume "8 0" -end "8 8" -fs %start_frame% -fe %end_frame%
