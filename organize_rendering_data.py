c220c5cebf3db0f6bee6c39fee094322e0d31249
catdance124 <kinosi.cat@gmail.com>
2020-08-13 17:22:04 +0900

    I have organized only a portion (horizontal/vertical) of the rendered data (to save on data size).

--- organize_rendering_data.py
+++ organize_rendering_data.py
@@ -18,21 +18,22 @@ for scene in l:
     print(scene)
     for iy in range(9):
         for ix in range(9):
-            for exr_path in glob.glob(f'{scene}/{iy:02}_{ix:02}/*.exr'):
-                # path settings
-                old_dir = os.path.dirname(exr_path).replace('\\', '/')
-                new_dir = old_dir.replace('../rendering/clean', '../data').replace('/9x9_baseline0.01', '')
-                frame = os.path.splitext(os.path.basename(exr_path))[0][1:]    # '**/**/Z0104.exr' -> '0104'
-                os.makedirs(new_dir, exist_ok=True)
+            if ix == 4 or iy ==4:
+                for exr_path in glob.glob(f'{scene}/{iy:02}_{ix:02}/*.exr'):
+                    # path settings
+                    old_dir = os.path.dirname(exr_path).replace('\\', '/')
+                    new_dir = old_dir.replace('../rendering/clean', '../data').replace('/9x9_baseline0.01', '')
+                    frame = os.path.splitext(os.path.basename(exr_path))[0][1:]    # '**/**/Z0104.exr' -> '0104'
+                    os.makedirs(new_dir, exist_ok=True)
 
-                if (not os.path.exists(f'{new_dir}/{frame}.npy')) and (not os.path.exists(f'{new_dir}/{frame}.png')):
-                    # EXR(depth) -> npy(disparity)
-                    img, Z = readEXR(exr_path)
-                    for scene_id, num in lt:
-                        if scene_id in old_dir:
-                            focal_length = float(num)
-                    disp = depth2disp(Z, baseline=0.01, focal_length=focal_length)
-                    np.save(f'{new_dir}/{frame}', disp)
+                    if (not os.path.exists(f'{new_dir}/{frame}.npy')) and (not os.path.exists(f'{new_dir}/{frame}.png')):
+                        # EXR(depth) -> npy(disparity)
+                        img, Z = readEXR(exr_path)
+                        for scene_id, num in lt:
+                            if scene_id in old_dir:
+                                focal_length = float(num)
+                        disp = depth2disp(Z, baseline=0.01, focal_length=focal_length)
+                        np.save(f'{new_dir}/{frame}', disp)
 
-                    # copy .png
-                    shutil.copy(f'{old_dir}/{frame}.png', new_dir)
+                        # copy .png
+                        shutil.copy(f'{old_dir}/{frame}.png', new_dir)
